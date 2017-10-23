1ea34afdc34164ceb8c84df47042261a37d2cb45
December2 <32958404+December2@users.noreply.github.com>
2017-10-23 18:20:11 -0400

    tonywilkins

---
+++ Web Page/main.js
@@ -0,0 +1,28 @@
+function clicked() {
+
+  var user = document.getElementById('username');
+  var pass = document.getElementById('password');
+
+  var coruser = "22tony.wilkins";
+  var corpass = "Gps202657";
+
+  if(user.value == coruser) {
+
+    if(pass.value == corpass) {
+
+      window.alert("You are logged in as " + user.value);
+
+    } else {
+
+      window.alert("Incorrect username or password!")
+
+    }
+
+  } else {
+
+          window.alert("Incorrect username or password!")
+
+  }
+
+
+}
